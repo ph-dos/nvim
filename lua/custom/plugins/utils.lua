b869df9ed70bd1d8d9a7735cd7e942f87be5c8f4
Ph-Dos <phm47860@uga.edu>
2026-01-11 17:20:58 -0600

    lil patch v2

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -3,7 +3,14 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = { use_default_keymaps = false },
+    opts = {
+      keymaps = {
+        ['<CR>'] = 'actions.select',
+        ['<C-c>'] = { 'actions.close', mode = 'n' },
+        ['.'] = { 'actions.toggle_hidden', mode = 'n' },
+      },
+      use_default_keymaps = false,
+    },
     lazy = false,
   },
   {
