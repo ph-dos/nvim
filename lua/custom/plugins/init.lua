b49200d24f77afedd69f5da5f9e532419c586c21
Ph-Dos <phm47860@uga.edu>
2026-01-12 14:32:49 -0600

    reduce

--- lua/custom/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-local utils = require 'custom.plugins.lsps'
-local lsps = require 'custom.plugins.lsps'
-local styling = require 'custom.plugins.styling'
-
-return {
-  unpack(utils),
-  unpack(lsps),
-  unpack(styling),
-}
