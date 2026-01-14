7a491e2aef2537549acc8711cb9751a9c7399a08
Ph-Dos <phm47860@uga.edu>
2026-01-14 02:51:22 -0600

    nvm

--- lua/custom/plugins/styling.lua
+++ lua/custom/plugins/styling.lua
@@ -8,14 +8,8 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      filter = 'spectrum',
-      transparent_background = true,
-      background_clear = { 'float_win', 'telescope', 'which-key', 'notify' },
-      override = function()
-        return {
-          BlinkCmpMenu = { bg = '#000000' },
-        }
-      end,
+      filter = 'ristretto',
+      background_clear = { 'float_win', 'telescope', 'which-key' },
     },
   },
 }
