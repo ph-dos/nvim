15a7552ff0f45f4b99e2bba4669dab8a83bd07bc
ph-dos <phm4860@uga.edu>
2026-01-17 15:20:41 -0600

    less lines

--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
+-- [[ Setting options ]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false
 vim.o.winborder = 'rounded'
 vim.o.guicursor = vim.o.guicursor .. ',i:block-Cursor'
 vim.o.guicursor = vim.o.guicursor .. ',n:block-blinkwait150-blinkon150-blinkoff150'
-
--- [[ Setting options ]]
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = false
@@ -22,16 +20,10 @@ vim.o.timeoutlen = 300
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
@@ -60,8 +52,6 @@ vim.keymap.set('n', '<Leader>e', '<CMD>Oil<CR>', { desc = 'Open parent directory
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Keybinds to make split navigation easier.
@@ -72,7 +62,6 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' 
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
-
 vim.keymap.set('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
 vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
 vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
@@ -91,7 +80,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -123,16 +111,12 @@ require('lazy').setup({
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    event = 'VimEnter',
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
-      -- this setting is independent of vim.o.timeoutlen
       delay = 0,
       icons = {
-        -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
-        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {
           Up = '<Up> ',
           Down = '<Down> ',
@@ -255,11 +239,7 @@ require('lazy').setup({
       { 'mason-org/mason.nvim', opts = {} },
       'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
     },
     config = function()
@@ -273,40 +253,30 @@ require('lazy').setup({
           end
 
           -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
           map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
           map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
           -- Find references for the word under your cursor.
           map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
           map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
           map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
           map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
           -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
           map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
           -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
@@ -328,14 +298,14 @@ require('lazy').setup({
         virtual_text = false,
         severity_sort = true,
         underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = {
+        signs = vim.g.have_nerd_font and {
           text = {
-            [vim.diagnostic.severity.ERROR] = vim.g.have_nerd_font and '󱈸' or '!',
-            [vim.diagnostic.severity.WARN] = vim.g.have_nerd_font and '󰋖' or '?',
-            [vim.diagnostic.severity.INFO] = vim.g.have_nerd_font and '' or 'I',
-            [vim.diagnostic.severity.HINT] = vim.g.have_nerd_font and '' or 'H',
+            [vim.diagnostic.severity.ERROR] = '󱈸',
+            [vim.diagnostic.severity.WARN] = '󰋖',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
           },
-        },
+        } or {},
       }
 
       local capabilities = require('blink.cmp').get_lsp_capabilities()
@@ -343,20 +313,14 @@ require('lazy').setup({
         basedpyright = {
           analysis = {
             typeCheckingMode = 'off',
-            inlayHints = {
-              genericTypes = true,
-              variableTypes = true,
-              functionReturnTypes = true,
-            },
+            inlayHints = { genericTypes = true, variableTypes = true, functionReturnTypes = true },
           },
         },
 
         lua_ls = {
           settings = {
             Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
+              completion = { callSnippet = 'Replace' },
               diagnostics = { disable = { 'missing-fields' } },
             },
           },
@@ -365,9 +329,7 @@ require('lazy').setup({
 
       -- Ensure the servers and tools above are installed
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
+      vim.list_extend(ensure_installed, { 'stylua' })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
@@ -407,16 +369,10 @@ require('lazy').setup({
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
+          return { timeout_ms = 500, lsp_format = 'fallback' }
         end
       end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'ruff_format', 'ruff_fix' },
-      },
+      formatters_by_ft = { lua = { 'stylua' }, python = { 'ruff_format', 'ruff_fix' } },
     },
   },
 
@@ -425,22 +381,17 @@ require('lazy').setup({
     event = 'VimEnter',
     version = '1.*',
     dependencies = {
-      -- Snippet Engine
       {
         'L3MON4D3/LuaSnip',
         version = '2.*',
         build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
+          -- Build Step is needed for regex support in snippets; this step is not supported in many windows environments.
           if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
             return
           end
           return 'make install_jsregexp'
         end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets. https://github.com/rafamadriz/friendly-snippets
-        },
+        -- dependencies = { `friendly-snippets` contains a variety of premade snippets. https://github.com/rafamadriz/friendly-snippets },
         opts = {},
       },
       'folke/lazydev.nvim',
@@ -466,7 +417,7 @@ require('lazy').setup({
       fuzzy = { implementation = 'lua' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = false },
     },
   },
 
@@ -476,30 +427,20 @@ require('lazy').setup({
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
@@ -536,7 +477,7 @@ require('lazy').setup({
         if error_count == 0 then
           return ''
         end
-        local icon = vim.g.have_nerd_font and '󱈸' or '!'
+        local icon = vim.g.have_nerd_font and '󱈸' or 'E'
         return string.format('%s %s', icon, error_count)
       end
 
@@ -552,12 +493,11 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       },
--- lua/custom/plugins/styling.lua
+++ lua/custom/plugins/styling.lua
@@ -10,7 +10,7 @@ return {
       vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'NONE', fg = 'NONE' })
       vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', fg = 'NONE' })
       vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE', fg = 'NONE' })
-      vim.api.nvim_set_hl(0, 'TelescopeMatching', { bold = true, italic = false, underline = true })
+      vim.api.nvim_set_hl(0, 'TelescopeMatching', { bold = true, italic = false, underdouble = true })
       vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = '#FFAA88' })
       vim.api.nvim_set_hl(0, 'MatchParen', { fg = '#ffffff', bold = true })
     end,
