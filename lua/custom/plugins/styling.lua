7c806985fe5e4d4066ef0fcb91d525bb6e29143c
ph-dos <phm4860@uga.edu>
2026-01-17 09:19:34 -0600

    more better

--- lua/custom/plugins/styling.lua
+++ lua/custom/plugins/styling.lua
@@ -10,8 +10,9 @@ return {
       vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'NONE', fg = 'NONE' })
       vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', fg = 'NONE' })
       vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE', fg = 'NONE' })
-      vim.api.nvim_set_hl(0, 'TelescopeMatching', { fg = '#ffffff', bold = true, italic = false })
+      vim.api.nvim_set_hl(0, 'TelescopeMatching', { bold = true, italic = false, underline = true })
       vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = '#FFAA88' })
+      vim.api.nvim_set_hl(0, 'MatchParen', { fg = '#ffffff', bg = 'NONE', bold = true })
     end,
   },
 }
