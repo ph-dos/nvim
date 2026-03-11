2f64e5e6dcf1604c8d402b064071be84977b974d
ph-dos <pmiller0706@gmail.com>
2026-03-10 22:05:02 -0500

    fix: tree shitter install issue

--- init.lua
+++ init.lua
@@ -293,7 +293,7 @@ require('lazy').setup({
           end
         end,
       })
-			
+
       vim.diagnostic.config {
         virtual_text = false,
         severity_sort = true,
@@ -480,8 +480,9 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
