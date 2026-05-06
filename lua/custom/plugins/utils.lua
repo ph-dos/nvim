1ad6ca8101d1d53a662a3b0762c449face861a92
ph-dos <pmiller0706@gmail.com>
2026-05-06 16:07:24 -0500

    feat: add html closing tage auto complete

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -45,4 +45,17 @@ return {
             { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
         },
     },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            opts.ensure_installed = opts.ensure_installed or {}
+            vim.list_extend(opts.ensure_installed, { "tsx", "typescript", "html", "javascript" })
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {},
+    },
 }
