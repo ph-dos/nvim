e3d6216431f06ef9cf0f3fbcc61300fc9a95f8d8
ph-dos <pmiller0706@gmail.com>
2026-05-29 14:11:25 -0500

    fix: set auto pairing to only curly braces

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -49,8 +49,10 @@ return {
         event = "InsertEnter",
         config = function()
             local npairs = require("nvim-autopairs")
+            local Rule = require("nvim-autopairs.rule")
             npairs.setup({})
-            npairs.remove_rule("(")
+            npairs.clear_rules()
+            npairs.add_rule(Rule("{", "}"))
         end,
     },
 }
